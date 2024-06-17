b15fba9aa6c3726173f32768f48aa32081785da5
Leo S <123216029+leojoamalan@users.noreply.github.com>
2024-06-17 16:16:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def download_and_load_model(url, model_path="best_model.pth"):
             return None
 
     try:
-       model = torch.load(io.BytesIO(response.content), map_location=torch.device('cpu'))
+        model = torch.load(io.BytesIO(response.content), map_location=torch.device('cpu'))
         model.eval()
         st.success("Model loaded successfully")
         return model
