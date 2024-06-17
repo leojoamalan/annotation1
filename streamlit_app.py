a4e7f5beb01b51c5120e429c52567aa924301cc2
Leo S <123216029+leojoamalan@users.noreply.github.com>
2024-06-17 15:43:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,18 +47,17 @@ def main():
     
     model_url ="https://github.com/leojoamalan/annotation/blob/main/best_model.pth"
     model = download_and_load_model(model_url)
-    
-    if model:
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
-            st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
             
-            st.write("Annotating...")
-            annotated_image, result_text = predict_and_annotate(image, model)
+        st.write("Annotating...")
+        annotated_image, result_text = predict_and_annotate(image, model)
             
-            st.image(annotated_image, caption='Annotated Image', use_column_width=True)
-            st.write(result_text)
+        st.image(annotated_image, caption='Annotated Image', use_column_width=True)
+        st.write(result_text)
 
 if __name__ == "__main__":
     main()
