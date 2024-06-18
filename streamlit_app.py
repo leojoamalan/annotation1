878a1f1e48567f9deacd7805557734c99fb51185
Leo S <123216029+leojoamalan@users.noreply.github.com>
2024-06-18 09:50:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ def main():
 
     if not os.path.exists(MODEL_PATH):
         st.info("Downloading model...")
-        response = requests.get(MODEL_URL)
+        response = requests.get(model_url)
         with open(MODEL_PATH, 'wb') as f:
             f.write(response.content)
         st.success("Model downloaded!") 
