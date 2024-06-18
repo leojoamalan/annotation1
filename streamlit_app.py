20d0c9f3c6d8192c2461e8076c9808b6d03143e6
Leo S <123216029+leojoamalan@users.noreply.github.com>
2024-06-18 09:49:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,16 @@ import requests
 import io
 
 @st.cache(allow_output_mutation=True)
-def load_model(model_url):
-    try:
-        response = requests.get(model_url)
-        response.raise_for_status()
-        model = torch.load(response.content, map_location=torch.device('cpu'))
-        model.eval()
-        return model
-    except Exception as e:
-        st.error(f"Failed to load model: {e}")
-        return None
+# def load_model(model_url):
+#     try:
+#         response = requests.get(model_url)
+#         response.raise_for_status()
+#         model = torch.load(response.content, map_location=torch.device('cpu'))
+#         model.eval()
+#         return model
+#     except Exception as e:
+#         st.error(f"Failed to load model: {e}")
+#         return None
 
 def preprocess_image(image):
     preprocess = transforms.Compose([
@@ -34,8 +34,19 @@ def main():
     st.title("Kidney Stone Annotation Tool")
     st.write("Upload an image to get started.")
     
-    model_url ="https://github.com/leojoamalan/annotation/blob/main/best_model.pth"
-    model = load_model(model_url)
+    model_url ="https://drive.google.com/uc?export=download&id=1MPkwGD6Jx0b63mvdCKdIBubtCDHMaZry"
+    # model = load_model(model_url)
+    MODEL_PATH = "best_model.pth"
+
+    if not os.path.exists(MODEL_PATH):
+        st.info("Downloading model...")
+        response = requests.get(MODEL_URL)
+        with open(MODEL_PATH, 'wb') as f:
+            f.write(response.content)
+        st.success("Model downloaded!") 
+    # Load your pre-trained model
+    model = torch.load(MODEL_PATH, map_location=torch.device('cpu'))
+    model.eval()
     if model:
         uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
         if uploaded_file is not None:
@@ -43,7 +54,7 @@ def main():
             st.image(image, caption='Uploaded Image', use_column_width=True)
                 
             st.write("Annotating...")
-            annotated_image = model.predict(preprocess_image,conf=0.35)
+            annotated_image = model.predict(preprocess_image(image),conf=0.35)
                 
             st.image(annotated_image.show(), caption='Annotated Image', use_column_width=True)
 
